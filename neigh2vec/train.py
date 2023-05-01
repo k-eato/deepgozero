617d5a3c4169adcc2e6eea1b7af8e2fa7f28f725
Kenneth Eaton <keaton30@login-coc-ice-2.pace.gatech.edu>
2023-05-01 19:17:51 -0400

    Added neigh2vec files

--- neigh2vec/train.py
+++ neigh2vec/train.py
@@ -1,5 +1,5 @@
-import anc2vec
-import anc2vec.train as builder
+import neigh2vec
+import neigh2vec.train as builder
 import pickle
 
 es = builder.fit('../data/go.obo', embedding_sz=1024, batch_sz=64, num_epochs=2)
